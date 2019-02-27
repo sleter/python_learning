c477b5b562fe85dcc6139f262ce4c16c38f956f1
sleter <sleter97@gmail.com>
2019-02-27 19:43:23 +0100

    webscraping basics

---
+++ webscraping/basic.py
@@ -0,0 +1,18 @@
+import bs4 as  bs
+import urllib.request
+
+sauce = urllib.request.urlopen("https://pythonprogramming.net/parsememcparseface/").read()
+soup = bs.BeautifulSoup(sauce,'lxml')
+
+# print(soup.title)
+# print(soup.name.title)
+# print(soup.title.string)
+# print(soup.p)
+
+# for p in soup.find_all('p'):
+#     print(p.string) # work when there is no child tags inside / use .text instead
+
+# print(soup.get_text()) # reads not only paragraphs
+
+for url in soup.find_all('a'):
+    print(url.get('href'))
